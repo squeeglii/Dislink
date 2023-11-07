1071a333c44f5cb15f481466a63b578ca4b30f4b
Will Scully <16578982+squeeglii@users.noreply.github.com>
2023-11-07 18:19:32 +0000

    Initial commit - added SqueeGradle

---
+++ src/main/java/me/squeeglii/plugin/dislink/Dislink.java
@@ -0,0 +1,17 @@
+package me.squeeglii.plugin.dislink;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dislink extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
